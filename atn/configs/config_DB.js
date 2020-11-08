ff93a805cc8d0dbcafe89f78067e30ab43d3327f
unknown <trinhngocphuc8@gmail.com>
2020-11-08 21:00:33 +0700

    update

---
+++ atn/configs/config_DB.js
@@ -0,0 +1,16 @@
+module.exports = {
+    localdb: {
+        urldb : 'mongodb+srv://phucngu123:phuc123@cluster0.nkzej.mongodb.net/atn-shop?retryWrites=true&w=majority',
+        dbname : "atn-shop",
+        dbusername : "",
+        dbpassword : "",
+    },
+
+    clouddb: {
+        urldb : 'mongodb+srv://phucngu123:phuc123@cluster0.nkzej.mongodb.net/atn-shop?retryWrites=true&w=majority',
+        dbname : "atn-shop",
+        dbusername : "",
+        dbpassword : "",
+    }
+
+};
